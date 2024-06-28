1f3df84bc5f8d8847cdf912d87b22d30dbda459a
Alexander Pechinkin <pechinkin04@yandex.ru>
2024-06-28 15:56:45 +0400

    Initial Commit

---
+++ ClipherVault/ClipherVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipherVaultApp.swift
+//  ClipherVault
+//
+//  Created by Александр Печинкин on 28.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ClipherVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
